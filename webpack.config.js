a0d3d588232fd2659262ba3a9d06b9ea0d6133b6
masad-frost <farismasad@gmail.com>
2019-10-16 15:22:33 -0700

    Remove references to monorepo in webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,56 +1,42 @@
-const path = require('path');
-const webpack = require('webpack');
-const HappyPack = require('happypack');
-const fs = require('fs');
-const os = require('os');
-const babelConfig = Object.assign(
-  {},
-  require('../../shared/babelConfig.json'),
-  {
-    cacheDirectory: true,
-  },
-);
+const path = require("path");
+const webpack = require("webpack");
+const HappyPack = require("happypack");
+const fs = require("fs");
+const os = require("os");
 
 const threadPool = HappyPack.ThreadPool({
-  size: os.cpus().length,
+  size: os.cpus().length
 });
 
 if (
-  ['production', 'development', 'test'].indexOf(process.env.NODE_ENV) === -1
+  ["production", "development", "test"].indexOf(process.env.NODE_ENV) === -1
 ) {
-  throw new Error('NODE_ENV is not set');
+  throw new Error("NODE_ENV is not set");
 }
 
-const prod = process.env.NODE_ENV === 'production';
-const bundleNamesFile = path.join(
-  __dirname,
-  '..',
-  '..',
-  '.webpack_bundle_names.json',
-);
+const prod = process.env.NODE_ENV === "production";
+const bundleNamesFile = path.join(__dirname, ".webpack_bundle_names.json");
 const bundleNames = fs.existsSync(bundleNamesFile)
   ? JSON.parse(fs.readFileSync(bundleNamesFile))
   : {};
-process.on('exit', () => {
+process.on("exit", () => {
   fs.writeFileSync(bundleNamesFile, JSON.stringify(bundleNames));
 });
 
-let version = 'dontcry';
+let version = "dontcry";
 if (prod) {
   if (!process.env.SOURCE_VERSION) {
-    console.error('Missing heroku release version');
+    console.error("Missing heroku release version");
     process.exit(1);
   }
 
   version = process.env.SOURCE_VERSION.slice(0, 7);
 }
 
-const monorepoRoot = process.env.ROOT_DIR || path.resolve(__dirname, '../../');
-
 function createConfig(name, entryPath) {
   const plugins = [
     function() {
-      this.plugin('done', stats => {
+      this.plugin("done", stats => {
         // For some reason happypack + --bail doesn't produce the correct exit code
         // when uglify fails. This makes sure it happens.
         if (
@@ -58,15 +44,15 @@ function createConfig(name, entryPath) {
           stats.compilation.errors &&
           stats.compilation.errors.length
         ) {
-          console.error(stats.compilation.errors.join('\n'));
-          process.on('beforeExit', () => {
+          console.error(stats.compilation.errors.join("\n"));
+          process.on("beforeExit", () => {
             process.exit(1);
           });
         }
 
         if (!stats.toJson().assetsByChunkName.main) {
-          console.error('Error getting main chunk for', name, entryPath);
-          console.error(stats.toJson().errors.join('\n'));
+          console.error("Error getting main chunk for", name, entryPath);
+          console.error(stats.toJson().errors.join("\n"));
           process.exit(1);
         }
 
@@ -75,32 +61,32 @@ function createConfig(name, entryPath) {
       });
     },
     new webpack.DefinePlugin({
-      'process.env': {
+      "process.env": {
         NODE_ENV: JSON.stringify(process.env.NODE_ENV),
         BROWSER: JSON.stringify(true),
         VERSION: JSON.stringify(version),
         SENTRY_DSN: JSON.stringify(process.env.SENTRY_DSN),
-        STAGING: JSON.stringify(process.env.STAGING),
-      },
-    }),
+        STAGING: JSON.stringify(process.env.STAGING)
+      }
+    })
   ];
 
-  if (prod && name === 'replbox_babel') {
+  if (prod && name === "replbox_babel") {
     // It hangs on the babel bundle. TODO: try updating webpack
     plugins.push(
       new webpack.optimize.UglifyJsPlugin({
         sourceMap: true,
-        compress: false,
-      }),
+        compress: false
+      })
     );
   } else if (prod) {
     plugins.push(
       new webpack.optimize.UglifyJsPlugin({
         sourceMap: true,
         compress: {
-          warnings: true,
-        },
-      }),
+          warnings: true
+        }
+      })
     );
   }
 
@@ -108,72 +94,73 @@ function createConfig(name, entryPath) {
     new HappyPack({
       loaders: [
         {
-          path: 'babel-loader',
-          query: babelConfig,
-        },
+          path: "babel-loader",
+          query: {
+            presets: ["env", "stage-2"],
+            babelrc: false,
+            cacheDirectory: true
+          }
+        }
       ],
       id: name,
       threadPool,
-      verbose: process.env.LOG_LEVEL === '0',
-    }),
+      verbose: process.env.LOG_LEVEL === "0"
+    })
   );
 
   return {
     watchOptions: {
       aggregateTimeout: 300,
-      poll: 500,
+      poll: 500
     },
     entry: entryPath,
     output: {
-      path: path.resolve(__dirname, '..', '..', 'public'),
-      filename: prod ? name + '.[chunkhash].bundle.js' : name + '.bundle.js',
+      path: path.resolve(__dirname, "..", "..", "public"),
+      filename: prod ? name + ".[chunkhash].bundle.js" : name + ".bundle.js",
       chunkFilename: prod
-        ? name + '.[chunkhash].[id].chunk.js'
-        : name + '.[id].chunk.js',
-      publicPath: '/public/',
+        ? name + ".[chunkhash].[id].chunk.js"
+        : name + ".[id].chunk.js",
+      publicPath: "/public/"
     },
-    devtool: 'source-map',
+    devtool: "source-map",
     resolveLoader: {
-      modules: [path.resolve(__dirname, 'node_modules'), 'node_modules'],
+      modules: [path.resolve(__dirname, "node_modules"), "node_modules"]
     },
     resolve: {
-      modules: [path.resolve(__dirname, 'node_modules'), 'node_modules'],
+      modules: [path.resolve(__dirname, "node_modules"), "node_modules"]
     },
     module: {
       rules: [
         {
           test: /\.js$/,
           include: [
-            path.resolve(__dirname, 'src'),
-            path.resolve(__dirname, 'integration_tests'),
-            path.resolve(monorepoRoot, 'packages', 'languages'),
-            path.resolve(monorepoRoot, 'packages', 'sentry-browser'),
-            path.resolve(monorepoRoot, 'packages', 'tracking'),
+            path.resolve(__dirname, "src"),
+            path.resolve(__dirname, "integration_tests")
           ],
-          use: 'happypack/loader?id=' + name,
-        },
-      ],
+          use: "happypack/loader?id=" + name
+        }
+      ]
     },
-    plugins,
+    plugins
   };
 }
 
 const configs = [];
 
-const langs = fs.readdirSync(path.resolve(__dirname, 'src', 'languages'));
+const langs = fs.readdirSync(path.resolve(__dirname, "src", "languages"));
 for (const lang of langs) {
-  const dir = path.resolve(__dirname, 'src', 'languages', lang);
+  const dir = path.resolve(__dirname, "src", "languages", lang);
   if (fs.statSync(dir).isDirectory()) {
-    configs.push(createConfig('replbox_' + lang, path.join(dir, 'index.js')));
+    configs.push(createConfig("replbox_" + lang, path.join(dir, "index.js")));
   }
 }
 
 if (!prod) {
   configs.push(
     createConfig(
-      'replbox_window',
-      path.join(__dirname, 'integration_tests', 'replbox_window.js'),
-    ),
+      "replbox_window",
+      path.join(__dirname, "integration_tests", "replbox_window.js")
+    )
   );
 }
 
