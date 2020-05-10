b43d86ad688f437b56cae5de70113c715e1ea37a
Amjad Masad <amjad.masad@gmail.com>
2020-05-10 19:03:42 +0000

    Remove throttle

--- src/languages/basic/display.js
+++ src/languages/basic/display.js
@@ -1,37 +1,6 @@
 function toPx(n) {
   return `${n}px`;
 }
-function throttle(func, wait, options) {
-  var context, args, result;
-  var timeout = null;
-  var previous = 0;
-  if (!options) options = {};
-  var later = function() {
-    previous = options.leading === false ? 0 : Date.now();
-    timeout = null;
-    result = func.apply(context, args);
-    if (!timeout) context = args = null;
-  };
-  return function() {
-    var now = Date.now();
-    if (!previous && options.leading === false) previous = now;
-    var remaining = wait - (now - previous);
-    context = this;
-    args = arguments;
-    if (remaining <= 0 || remaining > wait) {
-      if (timeout) {
-        clearTimeout(timeout);
-        timeout = null;
-      }
-      previous = now;
-      result = func.apply(context, args);
-      if (!timeout) context = args = null;
-    } else if (!timeout && options.trailing !== false) {
-      timeout = setTimeout(later, remaining);
-    }
-    return result;
-  };
-};
 
 function getPixelSize({ wrapper, columns, rows, borderWidth, }) {
   const wrapperSize = Math.min(wrapper.clientWidth, wrapper.clientHeight);
@@ -40,6 +9,7 @@ function getPixelSize({ wrapper, columns, rows, borderWidth, }) {
 
   return Math.floor((wrapperSize - totalBorderSize) / gridSize);
 }
+
 function createGrid({ wrapper, rows, columns, defaultBg, borderWidth, borderColor, }) {
   const params = {
     wrapper,
