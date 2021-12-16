3520a425b639e9f0e7c34a22029171d4c407682e
Zach Anderson <zach.inbox@gmail.com>
2021-12-16 02:47:39 -0600

    Migrate scheme to replbox cli

--- yarn.nix
+++ yarn.nix
@@ -10,6 +10,22 @@
       };
     }
     {
+      name = "biwascheme___biwascheme_0.7.4.tgz";
+      path = fetchurl {
+        name = "biwascheme___biwascheme_0.7.4.tgz";
+        url  = "https://registry.yarnpkg.com/biwascheme/-/biwascheme-0.7.4.tgz";
+        sha1 = "4c6fa17dcda81274baf5c3ca5f957312818e7576";
+      };
+    }
+    {
+      name = "optparse___optparse_1.0.5.tgz";
+      path = fetchurl {
+        name = "optparse___optparse_1.0.5.tgz";
+        url  = "https://registry.yarnpkg.com/optparse/-/optparse-1.0.5.tgz";
+        sha1 = "75e75a96506611eb1c65ba89018ff08a981e2c16";
+      };
+    }
+    {
       name = "prettier___prettier_1.19.1.tgz";
       path = fetchurl {
         name = "prettier___prettier_1.19.1.tgz";
